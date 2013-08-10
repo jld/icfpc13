98553377e17fae0f421fe6962f3475957f1c4892
Jed Davis <jld@panix.com>
2013-08-10 02:38:31 -0700

    Make the ALU shinier

--- lbv.js
+++ lbv.js
@@ -19,58 +19,59 @@ exports.word = 64;
 // inenb1 = ctl4
 // inenb2 = and(ctl2, ctl4)
 
-function mk_word(f, thisArg) {
-   var a = Array(word);
-   for (var i = 0; i < word; ++i) 
-      a[i] = f.call(thisArg, i);
+function iota(n, f, this_arg) {
+   var a = Array(n);
+   for (var i = 0; i < word; ++i)
+      a[i] = f.call(this_arg, i);
    return a;
 }
 
-function ALU(prob, x) {
-   var ctl = this.ctl = prob.mk_vars(5);
-   this.in0 = prob.mk_vars(word);
-   this.in1 = prob.mk_vars(word);
-   this.in2 = prob.mk_vars(word);
-   this.inenb0 = prob.mk_or([ctl[3], ctl[4]]);
-   this.inenb1 = ctl[4];
-   this.inenb2 = prob.mk_and([ctl[2], ctl[4]]);
+function mk_word(f, this_arg) {
+   iota(word, f, this_arg);
+}
 
-   var zero = mk_word(function() { return prob.mk_false() });
-   prob.eqn_if(-this.inenb0, this.in0, zero);
-   prob.eqn_if(-this.inenb1, this.in1, zero);
-   prob.eqn_if(-this.inenb2, this.in2, zero);
+function ALU(p, x) {
+   var ctl = this.ctl = p.mk_vars(5);
+   this.input = iota(3, function() { p.mk_vars(word) });
+   this.inenb = [p.mk_or([ctl[3], ctl[4]]),
+                 ctl[4],
+                 p.mk_and([ctl[2], ctl[4]])];
 
-   var tconst = mk_word(function(i) { return i == 0 ? this.ctl[0] : prob.mk_false() }, this);
-   var tnot = this.in0.map(function(a) { return -a });
-   var tshl1 = prob.mk_shift(this.in0, 1);
-   var tshr1 = prob.mk_shift(this.in0, -1);
-   var tshr4 = prob.mk_shift(this.in0, -4);
-   var tshr16 = prob.mk_shift(this.in0, -16);
-   var tand = prob.mk_andn([this.in0, this.in1]);
-   var tor = prob.mk_orn([this.in0, this.in1]);
-   var txor = prob.mk_xorn([this.in0, this.in1]);
-   var tplus = prob.mk_ripplecarry(this.in0, this.in1);
-   var tif0 = prob.mk_muxnn(this.in0, this.in1, this.in2);
+   var zero = mk_word(function() { return p.mk_false() });
+   for (var i = 0; i < 3; ++i)
+      p.eqn_if(-this.inenb[i], this.input[i], zero);
 
-   prob.implies([-ctl[4], -ctl[3], -ctl[2], ctl[1]], []);
-   prob.implies([ctl[2], ctl[1]], []);
-   prob.implies([ctl[2], ctl[0]], []);
-   prob.implies([ctl[4], ctl[3]], []);
+   var tconst = mk_word(function(i) { return i == 0 ? this.ctl[0] : p.mk_false() }, this);
+   var tnot = this.input[0].map(function(a) { return -a });
+   var tshl1 = p.mk_shift(this.input[0], 1);
+   var tshr1 = p.mk_shift(this.input[0], -1);
+   var tshr4 = p.mk_shift(this.input[0], -4);
+   var tshr16 = p.mk_shift(this.input[0], -16);
+   var tand = p.mk_andn([this.input[0], this.input[1]]);
+   var tor = p.mk_orn([this.input[0], this.input[1]]);
+   var txor = p.mk_xorn([this.input[0], this.input[1]]);
+   var tplus = p.mk_ripplecarry(this.input[0], this.input[1]);
+   var tif0 = p.mk_muxnn(this.input[0], this.input[1], this.input[2]);
 
-   var m00 = prob.mk_muxn(ctl[2], tconst, x);
+   p.implies([-ctl[4], -ctl[3], -ctl[2], ctl[1]], []);
+   p.implies([ctl[2], ctl[1]], []);
+   p.implies([ctl[2], ctl[0]], []);
+   p.implies([ctl[4], ctl[3]], []);
 
-   var msh1 = prob.mk_muxn(ctl[0], tshl1, tshr1);
-   var mshr = prob.mk_muxn(ctl[0], tshr4, tshr16);
-   var msh = prob.mk_muxn(ctl[1], msh1, mshr);
-   var m01 = prob.mk_muxn(ctl[2], msh, tnot);
+   var m00 = p.mk_muxn(ctl[2], tconst, x);
 
-   var mao = prob.mk_muxn(ctl[0], tand, tor);
-   var mxp = prob.mk_muxn(ctl[0], txor, tplus);
-   var mop2 = prob.mk_muxn(ctl[1], mao, mxp);
-   var m10 = prob.mk_muxn(ctl[2], mop2, tif0);
+   var msh1 = p.mk_muxn(ctl[0], tshl1, tshr1);
+   var mshr = p.mk_muxn(ctl[0], tshr4, tshr16);
+   var msh = p.mk_muxn(ctl[1], msh1, mshr);
+   var m01 = p.mk_muxn(ctl[2], msh, tnot);
 
-   var m0 = prob.mk_muxn(ctl[3], m00, m01);
-   this.out = prob.mk_muxn(ctl[4], m0, m10);
+   var mao = p.mk_muxn(ctl[0], tand, tor);
+   var mxp = p.mk_muxn(ctl[0], txor, tplus);
+   var mop2 = p.mk_muxn(ctl[1], mao, mxp);
+   var m10 = p.mk_muxn(ctl[2], mop2, tif0);
+
+   var m0 = p.mk_muxn(ctl[3], m00, m01);
+   this.output = p.mk_muxn(ctl[4], m0, m10);
 }
 exports.ALU = ALU; // for "unit testing"
 
